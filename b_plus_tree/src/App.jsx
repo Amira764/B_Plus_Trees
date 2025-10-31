d81a860ef2056de76aa07e494b061b5b0c5192bf
mariammahrous23 <mariammahrous23@gmail.com>
2025-10-31 12:26:34 +0200

    fix: moving to react project instead

---
+++ b_plus_tree/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p>
+        Upload the csv file to see B+ Tree visualization.
+      </p>
+    </>
+  )
+}
+
+export default App
